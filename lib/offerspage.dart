f3027fed01c99dd2a929c2a8a7d39dcf6502bda0
Jenil Desai <jenildev91@gmail.com>
2025-04-11 17:48:43 +0530

    v1.0.0
    + Boilerplate Code

---
+++ lib/offerspage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Offerspage extends StatelessWidget {
+  const Offerspage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Offer extends StatelessWidget {
+  const Offer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
